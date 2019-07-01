e82e689bd2b22899c6d7b0949515afc94073ea8c
Chip Schaff <cbschaff@ttic.edu>
2019-07-01 13:11:21 -0500

    ppo example

--- dl/examples/ppo/base.py
+++ dl/examples/ppo/base.py
@@ -6,56 +6,56 @@ import torch.nn as nn
 import numpy as np
 import gin
 
+
 @gin.configurable
-class NatureDQN(ActorCriticBase):
+class A3CCNN(ActorCriticBase):
     """
-    Deep network from https://www.nature.com/articles/nature14236
+    Deep network from https://arxiv.org/abs/1602.01783
     """
     def build(self):
-        self.conv1 = nn.Conv2d(4, 32, 8, 4)
-        self.conv2 = nn.Conv2d(32, 64, 4, 2)
-        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.conv1 = nn.Conv2d(4, 16, 8, 4)
+        self.conv2 = nn.Conv2d(16, 32, 4, 2)
         shape = self.observation_space.shape[1:]
-        for c in [self.conv1, self.conv2, self.conv3]:
+        for c in [self.conv1, self.conv2]:
             shape = conv_out_shape(shape, c)
-        self.nunits = 64 * np.prod(shape)
-        self.fc = nn.Linear(self.nunits, 512)
-        self.vf = nn.Linear(512, 1)
-        self.dist = Categorical(512, self.action_space.n)
+        self.nunits = 32 * np.prod(shape)
+        self.fc = nn.Linear(self.nunits, 256)
+        self.vf = nn.Linear(256, 1)
+        self.dist = Categorical(256, self.action_space.n)
+        nn.init.orthogonal_(self.vf.weight.data, gain=1.0)
+        nn.init.constant_(self.vf.bias.data, 0)
 
     def forward(self, x):
-        x = x.float() / 255.
+        x = (x.float() / 128.) - 1.
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
         x = F.relu(self.fc(x.view(-1, self.nunits)))
         return self.dist(x), self.vf(x)
 
 
 @gin.configurable
-class NatureDQNRNN(ActorCriticBase):
+class A3CRNN(ActorCriticBase):
     """
-    Deep network from https://www.nature.com/articles/nature14236
+    Deep network from https://arxiv.org/abs/1602.01783
     """
     def build(self):
-        self.conv1 = nn.Conv2d(4, 32, 8, 4)
-        self.conv2 = nn.Conv2d(32, 64, 4, 2)
-        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.conv1 = nn.Conv2d(4, 16, 8, 4)
+        self.conv2 = nn.Conv2d(16, 32, 4, 2)
         shape = self.observation_space.shape[1:]
-        for c in [self.conv1, self.conv2, self.conv3]:
+        for c in [self.conv1, self.conv2]:
             shape = conv_out_shape(shape, c)
-        self.nunits = 64 * np.prod(shape)
-        self.lstm = MaskedLSTM(self.nunits, 512, 1)
+        self.nunits = 32 * np.prod(shape)
+        self.fc = nn.Linear(self.nunits, 256)
+        self.lstm = MaskedLSTM(256, 256, 1)
         self.tbf = TimeAndBatchUnflattener()
-        self.vf = nn.Linear(512, 1)
-        self.dist = Categorical(512, self.action_space.n)
+        self.vf = nn.Linear(256, 1)
+        self.dist = Categorical(256, self.action_space.n)
 
     def forward(self, x, state_in=None, mask=None):
-        x = x.float() / 255.
+        x = (x.float() / 128.) - 1.
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, self.nunits)
+        x = F.relu(self.fc(x.view(-1, self.nunits)))
         if state_in is None:
             x, state_out = self.lstm(self.tbf(x))
         else:
--- dl/examples/ppo/train_rnn.sh
+++ dl/examples/ppo/train_rnn.sh
@@ -1,1 +1,1 @@
-python -m dl.train logs_rnn ./ppo.gin -b "Policy.base=@NatureDQNRNN"
+python -m dl.train logs_rnn ./ppo.gin -b "Policy.base=@A3CRNN"
