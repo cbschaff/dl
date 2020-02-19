cf5afc1d5d527b26ef6d361d106ccde01d28c455
Chip Schaff <cbschaff@ttic.edu>
2020-02-19 14:25:07 -0600

    added continuous action space example for ppo2

--- examples/ppo2/base.py
+++ examples/ppo2/base.py
@@ -1,15 +1,12 @@
 """Define networks for PPO2 experiments."""
 from dl.rl.modules import PolicyBase, ValueFunctionBase, Policy, ValueFunction
-from dl.rl.util import conv_out_shape
-from dl.modules import Categorical
+from dl.modules import Categorical, DiagGaussian
 import torch.nn.functional as F
 import torch.nn as nn
-from torch.nn.utils.rnn import PackedSequence
-import numpy as np
 import gin
 
 
-class FeedForwardPolicyBase(PolicyBase):
+class DiscretePolicyBase(PolicyBase):
     """Policy network."""
 
     def build(self):
@@ -17,6 +14,22 @@ class FeedForwardPolicyBase(PolicyBase):
         self.fc1 = nn.Linear(self.observation_space.shape[0], 128)
         self.fc2 = nn.Linear(128, 128)
         self.dist = Categorical(128, self.action_space.n)
+
+    def forward(self, x):
+        """Forward."""
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.dist(x)
+
+
+class ContinuousPolicyBase(PolicyBase):
+    """Policy network."""
+
+    def build(self):
+        """Build."""
+        self.fc1 = nn.Linear(self.observation_space.shape[0], 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.dist = DiagGaussian(128, self.action_space.shape[0])
 
     def forward(self, x):
         """Forward."""
@@ -42,9 +55,15 @@ class VFNet(ValueFunctionBase):
 
 
 @gin.configurable
-def policy_fn(env):
-    """Create a3c conv net policy."""
-    return Policy(FeedForwardPolicyBase(env.observation_space, env.action_space))
+def discrete_policy_fn(env):
+    """Create policy."""
+    return Policy(DiscretePolicyBase(env.observation_space, env.action_space))
+
+
+@gin.configurable
+def continuous_policy_fn(env):
+    """Create policy."""
+    return Policy(ContinuousPolicyBase(env.observation_space, env.action_space))
 
 
 @gin.configurable
--- examples/ppo2/train.sh
+++
@@ -1,1 +0,0 @@
-python -m dl.main logs ./ppo.gin
