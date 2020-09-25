81122be9e8080ae04d4a33f02cc1e2330f84ae12
Chip Schaff <cbschaff@ttic.edu>
2020-09-25 15:09:08 -0500

    add package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="dl",
+    version="0.0.1",
+    author="Charles Schaff",
+    author_email="cbschaff@ttic.edu",
+    description="Deep Reinforcement Learning Code",
+    url="https://github.com/cbschaff/dl",
+    license='MIT',
+    packages=['dl'],
+    install_requires=[
+      'numpy',
+      'matplotlib',
+      'imageio',
+      'pandas',
+      'gin-config',
+      'gym',
+      'gym[atari]',
+      'gym[box2d]',
+      'pyyaml==5.3.1'
+    ],
+    zip_safe=False,
+)
